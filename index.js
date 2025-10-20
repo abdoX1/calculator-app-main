f8f7fe5ab86292635185cd0289083277197ad46f
abdoX1 <125511739+abdoX1@users.noreply.github.com>
2025-10-20 21:49:46 +0100

    lay down the ground for the calc logic

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
-const switcher = document.querySelector(".switcher")
-const ball = document.querySelector(".moving-object")
-let ballCurrantLeftValue = 0
+(() => {
+  const switcher = document.querySelector(".switcher");
+  const ball = document.querySelector(".moving-object");
+  let ballCurrantLeftValue = 0;
 
-
-
-switcher.addEventListener("click", function (e) {
-  if (e.target.className == "theme-one") {
-    if (ballCurrantLeftValue != 0) {
-      ball.style.left = "5%"
-      ball.style.transform = "translate(-0%)"
-      ballCurrantLeftValue = 0
-      document.body.className = "theme"
+  switcher.addEventListener("click", function (e) {
+    if (e.target.className == "theme-one") {
+      if (ballCurrantLeftValue != 0) {
+        ball.style.left = "5%";
+        ball.style.transform = "translate(-0%)";
+        ballCurrantLeftValue = 0;
+        document.body.className = "theme";
+      }
     }
-  } 
-  if (e.target.className == "theme-two") {
-    if (ballCurrantLeftValue != 50) {
-      ball.style.left = "50%"
-      ball.style.transform = "translate(-50%)"
-      ballCurrantLeftValue = 50
-      document.body.className = "theme-one"
-    }
-  } 
-  if (e.target.className == "theme-three") {
-    if (ballCurrantLeftValue != 100) {
-      ball.style.left = "100%"
-      ball.style.transform = "translate(-110%)"
-      ballCurrantLeftValue = 100
-      document.body.className = "theme-two"
-    }
-  } 
-})
 
+    if (e.target.className == "theme-two") {
+      if (ballCurrantLeftValue != 50) {
+        ball.style.left = "50%";
+        ball.style.transform = "translate(-50%)";
+        ballCurrantLeftValue = 50;
+        document.body.className = "theme-one";
+      }
+    }
+
+    if (e.target.className == "theme-three") {
+      if (ballCurrantLeftValue != 100) {
+        ball.style.left = "100%";
+        ball.style.transform = "translate(-110%)";
+        ballCurrantLeftValue = 100;
+        document.body.className = "theme-two";
+      }
+    }
+  });
+
+  addEventListener("click", (e) => {
+    // this here is our calc logic //
+  })
+})();
