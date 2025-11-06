5d6e62b940afba723d588db7de13038b00965682
abdoX1 <125511739+abdoX1@users.noreply.github.com>
2025-11-06 22:24:12 +0100

    finished js on prototype level

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
   const ball = document.querySelector(".moving-object");
   let ballCurrantLeftValue = 0;
 
+
+
   switcher.addEventListener("click", function (e) {
     if (e.target.className == "theme-one") {
       if (ballCurrantLeftValue != 0) {
@@ -32,7 +34,46 @@
     }
   });
 
+  let output = document.querySelector(".output")
+  let calculation = ""
+  let containDot = false
+
   addEventListener("click", (e) => {
     // this here is our calc logic //
+    try {
+      if (!isNaN(Number(e.target.textContent))) {
+      output.textContent += e.target.textContent
+      calculation = output.textContent
+    }
+    if (e.target.className == "operation" && !/[+\-x/]$/.test(calculation)) {
+      output.textContent += " " + e.target.textContent + " "
+      calculation = output.textContent.trim()
+      containDot = false
+    }
+    if (e.target.className == "dot" && containDot == false) {
+      containDot = true
+      output.textContent += e.target.textContent
+      calculation = output.textContent.trim()
+    }
+    if (e.target.className == "del") {
+      if (output.textContent[(output.textContent.length - 1)] == " ") {
+        output.textContent = output.textContent.slice(0, -3)
+        calculation = output.textContent.trim()
+      } else {
+        output.textContent = output.textContent.slice(0, -1)
+        calculation = output.textContent.trim()
+      }
+    }
+    if (e.target.className == "reset") {
+      calculation = calculation.replace(/\s+/g, "");
+      output.textContent = ""
+      calculation = ""
+    }
+    if (e.target.className == "equal") {
+      output.textContent = eval(calculation.replace(/x/g, "*"))
+    }
+    } catch (error) {
+      alert("incorrect format, please try again")
+    }
   })
 })();
